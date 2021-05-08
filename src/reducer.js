203300b8672c1959606d0c7ae455033f8901c896
Md Nazmul Haque <bpinazmul18@gmail.com>
2021-05-09 04:43:28 +0600

    fetch data and store it

--- src/reducer.js
+++ src/reducer.js
@@ -11,6 +11,14 @@ const reducer = (state, action) => {
     case SET_LOADING:
       return { ...state, isLoading: true };
 
+    case SET_STORIES:
+      return {
+        ...state,
+        isLoading: false,
+        hits: action.payload.hits,
+        nbPages: action.payload.nbPages,
+      };
+
     default:
       throw new Error(`No matching ${action.type} action type`);
   }
